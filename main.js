6fe858d714b86e00a427d2949c0870eec7ac2557
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2021-08-28 15:18:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var images = [
+    "Family.png", "Dad.jpg", "Mom.png", "Me.jpg"
+];
+
+var names = [
+    ["Family book", "Dad", "Mom", "Me"]
+];
+
+var i = 0;
+
+function update() {
+    i++;
+    var numbers_of_family_member_in_array = 3
+    if (i > numbers_of_family_member_in_array) {
+        i = 0;
+    }
+    var updatedImage = images[i];
+    var updatedName = names[i];
+    document.getElementById("family_image").src = updatedImage;
+    document.getElementById("family_name").innerHTML = updatedName;
+}
